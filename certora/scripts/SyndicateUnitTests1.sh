48f346d525a80628e51663e3e238ae68656ceefd
teryanarmen <61996358+teryanarmen@users.noreply.github.com>
2023-02-03 08:28:15 -0800

    Create SyndicateUnitTests1.sh

---
+++ certora/scripts/SyndicateUnitTests1.sh
@@ -0,0 +1,19 @@
+
+        
+    certoraRun  certora/harnesses/SyndicateHarness.sol \
+        certora/harnesses/MockStakeHouseUniverse.sol \
+        certora/harnesses/MockStakeHouseRegistry.sol \
+        certora/harnesses/MockSlotSettlementRegistry.sol \
+        certora/harnesses/MocksETH.sol \
+        --verify SyndicateHarness:certora/specs/SyndicateUnitTests1.spec \
+        --cloud master \
+        --optimistic_loop \
+        --optimize 1 \
+        --loop_iter 3 \
+        $RULE \
+        --rule_sanity \
+        --settings -optimisticFallback=true \
+        --packages @blockswaplab=node_modules/@blockswaplab @openzeppelin=node_modules/@openzeppelin \
+        --msg "Syndicate $specFile $1 $2"
+
+   
